a6c09f63dc98feb5876ed185143050c1e89e922f
Kairav Parekh <kparekh01@gmail.com>
2016-08-25 11:53:49 -0400

    added attributes to devise sign up and added models to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+  def configure_permitted_parameters
+    added_attrs = [:user_name, :email, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs + [:first_name, :last_name, :birth_date, :bio])
+  end
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [)
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email, :password, :remember_me])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :birthday, :about, :email, :password, :current_password, :remember_me])
+  #end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821154950) do
+ActiveRecord::Schema.define(version: 20160825012234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
