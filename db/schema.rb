d466e1c3bf3987101662e8babae34e211285940e
Kairav Parekh <kparekh01@gmail.com>
2016-09-21 18:44:45 -0400

    Rooms Controller and added column to Message Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920063048) do
+ActiveRecord::Schema.define(version: 20160921224207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160920063048) do
   create_table "messages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "content"
   end
 
   create_table "relationships", force: :cascade do |t|
