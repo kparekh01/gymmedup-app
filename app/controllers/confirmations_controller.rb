511ab86930fe5f18a577e531915e1cbc04fb68c6
Kairav Parekh <kparekh01@gmail.com>
2016-09-27 12:11:06 -0400

    removed redirect from controller

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,7 +3,7 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
 
   def after_confirmation_path_for(resource_name, resource)
-    redirect_to "/gyms"
+    "/gyms"
   end
 
 end
