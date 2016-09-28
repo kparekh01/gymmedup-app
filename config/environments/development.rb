564df20d19c37750129a49415599f6c226be145e
Kairav Parekh <kparekh01@gmail.com>
2016-09-27 20:25:15 -0400

    changing devise confirm

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'http://gymmed-up.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'http://gymmed-up.herokuapp.com/', only_path: true}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   # In the development environment your application's code is reloaded on
