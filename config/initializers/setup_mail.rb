ad2d74638e564fd96b74febf24cb7a1f2ec11592
Kairav Parekh <kparekh01@gmail.com>
2017-01-04 19:40:17 -0500

    changed env variables to strings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address                => 'smtp.sendgrid.net',
   :port                   => 587,
   :authentication         => :plain,
-  :user_name              => ENV[SEND_GRID_USER_NAME],
-  :password               => ENV[SEND_GRID_PASSWORD],
+  :user_name              => ENV["SEND_GRID_USER_NAME"],
+  :password               => ENV["SEND_GRID_PASSWORD"],
   :domain                 => 'heroku.com',
   :enable_starttls_auto   => true
 }
