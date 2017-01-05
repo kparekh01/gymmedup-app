951dbe32768e8a0f09489497434db4dbb6a24351
Kairav Parekh <kparekh01@gmail.com>
2017-01-04 19:22:52 -0500

    Created ENV file

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address                => 'smtp.sendgrid.net',
   :port                   => 587,
   :authentication         => :plain,
-  :user_name              => 'app55486703@heroku.com',
-  :password               => 'jyllqgam9432',
+  :user_name              => ENV[SEND_GRID_USER_NAME],
+  :password               => ENV[SEND_GRID_PASSWORD],
   :domain                 => 'heroku.com',
   :enable_starttls_auto   => true
 }
